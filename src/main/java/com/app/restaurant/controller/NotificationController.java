c5217b410853d0894bb1fd310242849c2dd201ce
dusan.antic177@gmail.com <dusan.antic177@gmail.com>
2022-01-10 04:40:58 +0100

    Push notification implemented

--- src/main/java/com/app/restaurant/controller/NotificationController.java
+++ src/main/java/com/app/restaurant/controller/NotificationController.java
@@ -4,9 +4,11 @@ import com.app.restaurant.model.Notifications;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "/api")
 public class NotificationController {
 
     @Autowired
@@ -14,6 +16,40 @@ public class NotificationController {
 
     // Initialize Notifications
     private Notifications notifications = new Notifications(0);
+
+    @GetMapping("/notifyWaiter")
+    public String getNotificationWaiter() {
+
+        // Increment Notification by one
+        notifications.increment();
+
+        // Push notifications to front-end
+        template.convertAndSend("/topic/notification/waiter", notifications);
+
+        return "Notifications successfully sent to Angular !";
+    }
+    @GetMapping("/notifyCook")
+    public String getNotificationCook() {
+
+        // Increment Notification by one
+        notifications.increment();
+
+        // Push notifications to front-end
+        template.convertAndSend("/topic/notification/cook", notifications);
+
+        return "Notifications successfully sent to Angular !";
+    }
+    @GetMapping("/notifyBartender")
+    public String getNotificationBartender() {
+
+        // Increment Notification by one
+        notifications.increment();
+
+        // Push notifications to front-end
+        template.convertAndSend("/topic/notification/bartender", notifications);
+
+        return "Notifications successfully sent to Angular !";
+    }
 
     @GetMapping("/notify")
     public String getNotification() {
